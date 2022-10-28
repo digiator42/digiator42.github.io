f4d369088c4dfdf1e133bd8901e7f8265845d6da
digiator42 <digiator2020@gmail.com>
2022-10-29 00:09:20 +0400

    switched countries data to the bottom of the file

--- world_countries_data_visualization/data.js
+++ world_countries_data_visualization/data.js
@@ -1,3 +1,175 @@
+
+Chart.defaults.scale.gridLines.display = false; //default chart none gridline
+Chart.defaults.scale.gridLines.drawBorder = false; ///default chart none drawBorder
+
+const left_yValues_div = document.getElementById("left-yValues-column");
+const right_yValues_div = document.getElementById("right-yValues-column");
+const left_yValues_spans = document.querySelectorAll("#left-yValues-column > span");
+const right_yValues_spans = document.querySelectorAll("#right-yValues-column > span");
+
+//*********************getting & displaying languages data**********************/
+
+const languages_data = function languages_data() {
+  // creating a new array for all languages
+  let languagesArray = [];
+  let j = 0;
+  //looping through each element and storing languages one by one in languagesArray
+  countries_data
+    .map((element) => element.languages)
+    .forEach((element) => {
+      element.forEach((element) => {
+        languagesArray[j] = element.toString().split(",").toString();
+        j++;
+      });
+    });
+  //creating a new set of languages array for unique values
+  const set = new Set(languagesArray);
+
+  //comparing set(unique values) // each unique value with original array to get frequency of each language
+  let filtered_spoken_langugages = [];
+  set.forEach((language) =>filtered_spoken_langugages.push({
+      lang: language, count: languagesArray.filter((element) => element === language).length})
+  );
+  let most_spoken_langugages = filtered_spoken_langugages.sort((a, b) => b.count - a.count);
+
+  let left_yValues_column = [];
+  let right_yValues_column = [];
+
+  for (let i = 0; i < 10; i++) {
+    left_yValues_column[i] = most_spoken_langugages[i].lang;
+    right_yValues_column[i] = most_spoken_langugages[i].count;
+  }
+  return { left_yValues_column, right_yValues_column };
+};
+
+document.getElementById("languages-btn").onclick =
+  function display_languages() {
+	//preventing canvas overlapping on each other, each time trigger button creates a new chart
+    reset_canvas();
+
+    j = 0;
+    for (const span of left_yValues_spans) {
+      left_yValues_spans[j].innerHTML = languages_data().left_yValues_column[j];
+      j++;
+    }
+    j = 0;
+    for (const span of right_yValues_spans) {
+      right_yValues_spans[j].innerHTML = languages_data().right_yValues_column[j];
+      j++;
+    }
+
+    var xValues = languages_data().left_yValues_column;
+    var yValues = languages_data().right_yValues_column;
+    var barColors = "#bba802";
+
+    createChart(xValues, yValues, barColors);
+  };
+
+//*********************getting & displaying population data**********************/
+
+const populationData = function population_data() {
+  let populationArray =
+    //looping through each element and storing popualtion one by one in populationArray
+    countries_data.map((element) => {
+      return { country: element.name, population: element.population };
+    });
+
+  let most_populated_countries = populationArray.sort(
+    (a, b) => b.population - a.population
+  );
+
+  let left_yValues_column = [];
+  let right_yValues_column = [];
+
+  for (let i = 0; i < 10; i++) {
+    left_yValues_column[i] = most_populated_countries[i].country;
+    right_yValues_column[i] = most_populated_countries[i].population;
+  }
+
+  return { left_yValues_column, right_yValues_column };
+};
+
+document.getElementById("populaton-btn").onclick =
+  function display_population() {
+    
+	reset_canvas();
+
+    j = 0;
+    for (const span of left_yValues_spans) {
+      left_yValues_spans[j].innerHTML = populationData().left_yValues_column[j];
+      j++;
+    }
+    j = 0;
+    for (const span of right_yValues_spans) {
+      right_yValues_spans[j].innerHTML = populationData().right_yValues_column[j].toLocaleString();
+      j++;
+    }
+
+    var xValues = populationData().left_yValues_column;
+    var yValues = populationData().right_yValues_column;
+    var barColors = "#bba802";
+
+    createChart(xValues, yValues, barColors);
+  };
+//resetting canvas, stop overlapping
+function reset_canvas() {
+  document.getElementById("myChart").remove();
+  const canvas = document.createElement("canvas");
+  canvas.id = "myChart";
+  canvas.style.cssText = `max-width: 600px; 
+							height: 300px;
+							display: inline-block;`;
+  const div_container = document.getElementById("data-container");
+  let right_column = document.getElementById("right-yValues-column");
+  div_container.insertBefore(canvas, right_column);
+  const iframe_canvas = document.querySelector("iframe");
+  if (iframe_canvas) 
+  	div_container.removeChild(iframe_canvas);
+}
+
+function createChart(xValues, yValues, barColors) {
+  new Chart("myChart", {
+    type: "horizontalBar",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      scaleShowLabels: false,
+      legend: { display: false },
+      title: {
+        display: false,
+        text: "",
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              display: false, //this will remove only the label
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              display: false, //this will remove only the label
+            },
+          },
+        ],
+      },
+    },
+  });
+}
+
+
+
+
+
 const countries_data = [
   {
     name: "Afghanistan",
@@ -2256,170 +2428,3 @@ const countries_data = [
     area: 390757,
   },
 ];
-
-Chart.defaults.scale.gridLines.display = false; //default chart none gridline
-Chart.defaults.scale.gridLines.drawBorder = false; ///default chart none drawBorder
-
-const left_yValues_div = document.getElementById("left-yValues-column");
-const right_yValues_div = document.getElementById("right-yValues-column");
-const left_yValues_spans = document.querySelectorAll("#left-yValues-column > span");
-const right_yValues_spans = document.querySelectorAll("#right-yValues-column > span");
-
-//*********************getting & displaying languages data**********************/
-
-const languages_data = function languages_data() {
-  // creating a new array for all languages
-  let languagesArray = [];
-  let j = 0;
-  //looping through each element and storing languages one by one in languagesArray
-  countries_data
-    .map((element) => element.languages)
-    .forEach((element) => {
-      element.forEach((element) => {
-        languagesArray[j] = element.toString().split(",").toString();
-        j++;
-      });
-    });
-  //creating a new set of languages array for unique values
-  const set = new Set(languagesArray);
-
-  //comparing set(unique values) // each unique value with original array to get frequency of each language
-  let filtered_spoken_langugages = [];
-  set.forEach((language) =>filtered_spoken_langugages.push({
-      lang: language, count: languagesArray.filter((element) => element === language).length})
-  );
-  let most_spoken_langugages = filtered_spoken_langugages.sort((a, b) => b.count - a.count);
-
-  let left_yValues_column = [];
-  let right_yValues_column = [];
-
-  for (let i = 0; i < 10; i++) {
-    left_yValues_column[i] = most_spoken_langugages[i].lang;
-    right_yValues_column[i] = most_spoken_langugages[i].count;
-  }
-  return { left_yValues_column, right_yValues_column };
-};
-
-document.getElementById("languages-btn").onclick =
-  function display_languages() {
-	//preventing canvas overlapping on each other, each time trigger button creates a new chart
-    reset_canvas();
-
-    j = 0;
-    for (const span of left_yValues_spans) {
-      left_yValues_spans[j].innerHTML = languages_data().left_yValues_column[j];
-      j++;
-    }
-    j = 0;
-    for (const span of right_yValues_spans) {
-      right_yValues_spans[j].innerHTML = languages_data().right_yValues_column[j];
-      j++;
-    }
-
-    var xValues = languages_data().left_yValues_column;
-    var yValues = languages_data().right_yValues_column;
-    var barColors = "#bba802";
-
-    createChart(xValues, yValues, barColors);
-  };
-
-//*********************getting & displaying population data**********************/
-
-const populationData = function population_data() {
-  let populationArray =
-    //looping through each element and storing popualtion one by one in populationArray
-    countries_data.map((element) => {
-      return { country: element.name, population: element.population };
-    });
-
-  let most_populated_countries = populationArray.sort(
-    (a, b) => b.population - a.population
-  );
-
-  let left_yValues_column = [];
-  let right_yValues_column = [];
-
-  for (let i = 0; i < 10; i++) {
-    left_yValues_column[i] = most_populated_countries[i].country;
-    right_yValues_column[i] = most_populated_countries[i].population;
-  }
-
-  return { left_yValues_column, right_yValues_column };
-};
-
-document.getElementById("populaton-btn").onclick =
-  function display_population() {
-    
-	reset_canvas();
-
-    j = 0;
-    for (const span of left_yValues_spans) {
-      left_yValues_spans[j].innerHTML = populationData().left_yValues_column[j];
-      j++;
-    }
-    j = 0;
-    for (const span of right_yValues_spans) {
-      right_yValues_spans[j].innerHTML = populationData().right_yValues_column[j].toLocaleString();
-      j++;
-    }
-
-    var xValues = populationData().left_yValues_column;
-    var yValues = populationData().right_yValues_column;
-    var barColors = "#bba802";
-
-    createChart(xValues, yValues, barColors);
-  };
-//resetting canvas, stop overlapping
-function reset_canvas() {
-  document.getElementById("myChart").remove();
-  const canvas = document.createElement("canvas");
-  canvas.id = "myChart";
-  canvas.style.cssText = `max-width: 600px; 
-							height: 300px;
-							display: inline-block;`;
-  const div_container = document.getElementById("data-container");
-  let right_column = document.getElementById("right-yValues-column");
-  div_container.insertBefore(canvas, right_column);
-  const iframe_canvas = document.querySelector("iframe");
-  if (iframe_canvas) 
-  	div_container.removeChild(iframe_canvas);
-}
-
-function createChart(xValues, yValues, barColors) {
-  new Chart("myChart", {
-    type: "horizontalBar",
-    data: {
-      labels: xValues,
-      datasets: [
-        {
-          backgroundColor: barColors,
-          data: yValues,
-        },
-      ],
-    },
-    options: {
-      scaleShowLabels: false,
-      legend: { display: false },
-      title: {
-        display: false,
-        text: "",
-      },
-      scales: {
-        xAxes: [
-          {
-            ticks: {
-              display: false, //this will remove only the label
-            },
-          },
-        ],
-        yAxes: [
-          {
-            ticks: {
-              display: false, //this will remove only the label
-            },
-          },
-        ],
-      },
-    },
-  });
-}
