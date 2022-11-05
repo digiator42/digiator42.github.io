e8956bb6872fab4cfb6735ac0fb3d08d06a7ce35
digiator42 <digiator2020@gmail.com>
2022-11-06 00:36:40 +0400

    changed add_player eventListner, created onClick function once target button is clicked, created sort function according to scores values

--- mini_project_leaderboard/main.js
+++ mini_project_leaderboard/main.js
@@ -4,37 +4,73 @@ function myFunction() {
   const sName_input = document.getElementById("sName");
   const country_input = document.getElementById("country");
   const pScore_input = document.getElementById("pScore");
+  const holder_container = document.getElementById("holder");
+  let j = 0;
+  let scores = [];
 
   add_player.addEventListener("click", () => {
     let fName_value = fName_input.value;
     let sName_value = sName_input.value;
     let country_value = country_input.value;
     let pScore_value = pScore_input.value;
+    const new_leader_board = `<div id="container ${j}">
+                              <div id="name-date">
+                              <p>${fName_value} ${sName_value}</p>
+                              <p>date</p>
+                              </div>
+                              <div id="country">${country_value}</div>
+                              <div id="score">${pScore_value}</div>
+                              <div id="buttons">
+                              <button id="delete">del</button>
+                              <button id="increase-score">++</button>
+                              <button id="decrease-score">---</button>
+                              </div></div>`;
 
-    const new_leader_board = `<div id="container">
-<div id="name-date">
-<p>${fName_value} ${sName_value}</p>
-<p>date</p>
-</div>
-<div id="country">${country_value}</div>
-<div id="score">${pScore_value}</div>
-<div id="buttons">
-<button>del</button>
-<button>++</button>
-<button>---</button>
-</div></div>`;
-const holder_container = document.getElementById("holder");
-const leader_container = document.getElementById("container");
-const name_date = document.getElementById("name-date");
-const buttons = document.getElementById("buttons");
-const next_sibling = document.getElementById("next-sibling");
-    if (!leader_container) {
-      console.log("run1")
-      holder_container.innerHTML = new_leader_board;
-    } 
-    else{
-      next_sibling.insertAdjacentHTML("beforeBegin", new_leader_board);
-    }
-
+    holder_container.insertAdjacentHTML("beforeend", new_leader_board);
+    j++;
+    sort_items();
   });
+
+  if (document.addEventListener) {
+    document.addEventListener("click", onClick);
+  }
+  function onClick (event) {
+    var target = event.target ? event.target : event.srcElement;
+
+    if (target.id == "delete") {
+        target.parentNode.parentNode.remove();
+        console.log(1);
+        sort_items();
+    }
+    else if(target.id == "increase-score"){
+      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) + 5;
+      target.parentNode.previousElementSibling.innerHTML = result;
+      console.log(result);
+      sort_items();
+    }
+    else if(target.id == "decrease-score"){
+      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) - 5;
+      target.parentNode.previousElementSibling.innerHTML = result;
+      console.log(result);
+      sort_items();
+    }
+  }
+
+
+function sort_items(){
+  let all_leader_containers = document.querySelectorAll('#holder > div');
+  console.log(all_leader_containers);
+
+  for (let i = 0; i < all_leader_containers.length; i++) {
+    scores.push({container : all_leader_containers[i], 
+      score : parseInt(all_leader_containers[i].children[2].innerHTML)});
+  }
+  scores.sort((a, b) => b.score - a.score);
+  console.log(scores)
+  holder_container.innerHTML = '';
+  for (let i = 0; i < scores.length; i++) {
+    holder_container.appendChild(scores[i].container);
+  }
+  scores = [];
+  }
 }
