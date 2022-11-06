4be0709988c37d3e5b2f66209530abf1bc0b368f
digiator42 <digiator2020@gmail.com>
2022-11-06 04:34:13 +0400

    validating inputs by conditional statements

--- mini_project_leaderboard/main.js
+++ mini_project_leaderboard/main.js
@@ -1,4 +1,5 @@
 function myFunction() {
+  const error_msg = document.getElementById("error-msg");
   const add_player = document.getElementById("add-player");
   const fName_input = document.getElementById("fName");
   const sName_input = document.getElementById("sName");
@@ -8,10 +9,41 @@ function myFunction() {
   let j = 0;
   let scores = [];
 
+  add_player.addEventListener("click", () => {
+    let fName_value = fName_input.value;
+    let sName_value = sName_input.value;
+    let country_value = country_input.value;
+    let pScore_value = pScore_input.value;
+    const new_leader_board = `<div id="container ${j}">
+                              <div id="name-date">
+                              <p>${fName_value} ${sName_value}</p>
+                              <p id="date">${current_timings()}</p>
+                              </div>
+                              <div id="country">${country_value}</div>
+                              <div id="score">${pScore_value}</div>
+                              <div id="buttons">
+                              <button id="delete">del</button>
+                              <button id="increase-score">++</button>
+                              <button id="decrease-score">---</button>
+                              </div></div>`;
+
+    if(!fName_value || !sName_value || !country_value || !pScore_value)
+      error_msg.innerHTML = 'All fields are required'
+    else if(isNaN(pScore_value) || pScore_value < 0)
+      error_msg.innerHTML = 'Score must be a positive number'
+    else{
+    error_msg.innerHTML = '';
+    holder_container.insertAdjacentHTML("beforeend", new_leader_board);
+    j++;
+    sort_items();
+    }
+  });
+
+
   if (holder_container.addEventListener) {
     holder_container.addEventListener("click", onClick);
   }
-  function onClick (event) {
+  function onClick(event) {
     var target = event.target ? event.target : event.srcElement;
 
     if (target.id == "delete") {
@@ -31,43 +63,21 @@ function myFunction() {
     sort_items();
   }
 
-  add_player.addEventListener("click", () => {
-    let fName_value = fName_input.value;
-    let sName_value = sName_input.value;
-    let country_value = country_input.value;
-    let pScore_value = pScore_input.value;
-    const new_leader_board = `<div id="container ${j}">
-                              <div id="name-date">
-                              <p>${fName_value} ${sName_value}</p>
-                              <p id="date">${current_timings()}</p>
-                              </div>
-                              <div id="country">${country_value}</div>
-                              <div id="score">${pScore_value}</div>
-                              <div id="buttons">
-                              <button id="delete">del</button>
-                              <button id="increase-score">++</button>
-                              <button id="decrease-score">---</button>
-                              </div></div>`;
-    holder_container.insertAdjacentHTML("beforeend", new_leader_board);
-    j++;
-    sort_items();
-  });
+  function sort_items(){
+    let all_leader_containers = document.querySelectorAll('#holder > div');
+    console.log(all_leader_containers);
 
-function sort_items(){
-  let all_leader_containers = document.querySelectorAll('#holder > div');
-  console.log(all_leader_containers);
-
-  for (let i = 0; i < all_leader_containers.length; i++) {
-    scores.push({container : all_leader_containers[i], 
-      score : parseInt(all_leader_containers[i].children[2].innerHTML)});
-  }
-  scores.sort((a, b) => b.score - a.score);
-  console.log(scores)
-  holder_container.innerHTML = '';
-  for (let i = 0; i < scores.length; i++) {
-    holder_container.appendChild(scores[i].container);
-  }
-  scores = [];
+    for (let i = 0; i < all_leader_containers.length; i++) {
+      scores.push({container : all_leader_containers[i], 
+        score : parseInt(all_leader_containers[i].children[2].innerHTML)});
+    }
+    scores.sort((a, b) => b.score - a.score);
+    console.log(scores)
+    holder_container.innerHTML = '';
+    for (let i = 0; i < scores.length; i++) {
+      holder_container.appendChild(scores[i].container);
+    }
+    scores = [];
   }
 
   function current_timings(){
