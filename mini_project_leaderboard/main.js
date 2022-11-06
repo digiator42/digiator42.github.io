14d104424ce6e378545eaead60586474c6d6870a
digiator42 <digiator2020@gmail.com>
2022-11-06 04:18:15 +0400

    add date function on new_leader_board

--- mini_project_leaderboard/main.js
+++ mini_project_leaderboard/main.js
@@ -8,6 +8,29 @@ function myFunction() {
   let j = 0;
   let scores = [];
 
+  if (holder_container.addEventListener) {
+    holder_container.addEventListener("click", onClick);
+  }
+  function onClick (event) {
+    var target = event.target ? event.target : event.srcElement;
+
+    if (target.id == "delete") {
+        target.parentNode.parentNode.remove();
+        console.log(1);
+    }
+    else if(target.id == "increase-score"){
+      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) + 5;
+      target.parentNode.previousElementSibling.innerHTML = result;
+      console.log(result);
+    }
+    else if(target.id == "decrease-score"){
+      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) - 5;
+      target.parentNode.previousElementSibling.innerHTML = result;
+      console.log(result);
+    }
+    sort_items();
+  }
+
   add_player.addEventListener("click", () => {
     let fName_value = fName_input.value;
     let sName_value = sName_input.value;
@@ -16,7 +39,7 @@ function myFunction() {
     const new_leader_board = `<div id="container ${j}">
                               <div id="name-date">
                               <p>${fName_value} ${sName_value}</p>
-                              <p>date</p>
+                              <p id="date">${current_timings()}</p>
                               </div>
                               <div id="country">${country_value}</div>
                               <div id="score">${pScore_value}</div>
@@ -25,37 +48,10 @@ function myFunction() {
                               <button id="increase-score">++</button>
                               <button id="decrease-score">---</button>
                               </div></div>`;
-
     holder_container.insertAdjacentHTML("beforeend", new_leader_board);
     j++;
     sort_items();
   });
-
-  if (document.addEventListener) {
-    document.addEventListener("click", onClick);
-  }
-  function onClick (event) {
-    var target = event.target ? event.target : event.srcElement;
-
-    if (target.id == "delete") {
-        target.parentNode.parentNode.remove();
-        console.log(1);
-        sort_items();
-    }
-    else if(target.id == "increase-score"){
-      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) + 5;
-      target.parentNode.previousElementSibling.innerHTML = result;
-      console.log(result);
-      sort_items();
-    }
-    else if(target.id == "decrease-score"){
-      let result = parseInt(target.parentNode.previousElementSibling.innerHTML) - 5;
-      target.parentNode.previousElementSibling.innerHTML = result;
-      console.log(result);
-      sort_items();
-    }
-  }
-
 
 function sort_items(){
   let all_leader_containers = document.querySelectorAll('#holder > div');
@@ -72,5 +68,22 @@ function sort_items(){
     holder_container.appendChild(scores[i].container);
   }
   scores = [];
+  }
+
+  function current_timings(){
+    const now = new Date();
+    let seconds = now.getSeconds();
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+    let day = now.toLocaleDateString();
+    let month = now.toLocaleDateString('en-US', { month: 'long' });
+    let year = now.getFullYear();
+    console.log(day)
+    if(hours < 10)
+      hours = '0' + hours;
+    if(minutes < 10)
+      minutes = '0' + minutes;  
+
+    return month.slice(0, 3) + " " + day.slice(0,2) + "," + year + " " + hours + ":" + minutes;
   }
 }
